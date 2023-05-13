52a3adc23e1b1a9b1e733c9322cb153b75a080b7
Jim <pnwmakes@gmail.com>
2023-05-13 09:07:28 -0700

    stuff

--- App.js
+++ App.js
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import { StatusBar } from 'expo-status-bar';
-// import HomeScreen from './screens/HomeScreen';
-
-// export default function App() {
-//     return (
-//         <>
-//             <HomeScreen />
-//             <StatusBar style='auto' />
-//         </>
-//     );
-// }
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
